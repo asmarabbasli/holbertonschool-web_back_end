d446254328b39f2e877d1920d3c4dbed91b47de0
Your Name <you@example.com>
2025-11-14 20:20:45 +0400

    fix

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,3 +2,12 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     return re.sub(rf'({"|".join(fields)})=[^ {separator}]*', lambda m: f"{m.group(1)}={redaction}", message)
+
+fields = ["name", "email", "phone", "ssn", "password"]
+messages = [
+    "name=egg;email=egg@sample.com;password=eggcellent;date_of_birth=12/12/1986;",
+    "name=bob;email=bob@example.com;password=bob123;date_of_birth=03/04/1993;"
+]
+
+for message in messages:
+    print(filter_datum(fields, 'xxx', message, ';'))
