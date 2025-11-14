d2dd1cb359a4dfb5e6fbae93b6da9b4151a9d162
Your Name <you@example.com>
2025-11-14 20:45:47 +0400

    fix

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Module for filtering sensitive data from log messages.
 """
 
 import re
+import logging
 from typing import List
 
 
@@ -23,3 +24,32 @@ def filter_datum(fields: List[str], redaction: str,
     """
     pattern = rf'({"|".join(fields)})=[^ {separator}]*'
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original, self.SEPARATOR)
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+import logging
+
+RedactingFormatter = __import__('filtered_logger').RedactingFormatter
+
+message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+formatter = RedactingFormatter(fields=["email", "ssn", "password"])
+print(formatter.format(log_record))
